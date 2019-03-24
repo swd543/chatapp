5997f7a84cae89389bad952f1bf783b78c021145
Buga <swd543@gmail.com>
2019-03-24 21:19:45 +0530

    Added grotsque sound

--- src/components/SubscribedComponent.tsx
+++ src/components/SubscribedComponent.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from "react";
 import "./Component.css";
+import audio from "./message.wav";
 import { subscribe } from "mqtt-react";
 
 interface IProps {
@@ -7,8 +8,14 @@ interface IProps {
   data: any[];
 }
 
+const playAudio = () => {
+  var audioObject: any = new Audio(audio);
+  audioObject.play();
+};
+
 const SubscribedComponent: FunctionComponent<IProps> = props => {
   const { data } = props;
+  playAudio();
   return (
     <div className="Component">
       {data && data.map((d, i) => <li key={i}>{d}</li>)}
